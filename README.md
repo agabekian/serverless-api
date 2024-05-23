3f29bf5c3f616c94c90431637c0a091470f4f1dc
Armen Agabekian <2008nv@gmail.com>
2024-05-22 21:17:09 -0700

    oh-boy

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # serverless-api
+![uml.png](uml.png)
+Create api-gateway router
+
+Create Lambda (s)
+
+Recycle a “master” permission for lambda, so it can access dynamoDB. Add permissions if none for DB.
+Add permissions dropdown:   attach policy  . Then search/select/add
+
+Stages : like a branch, so DEV for all
+
+Crete new METHOD(not res) while clicking on people/ entry  for CREATE, etc
+
+GET people/
+![getpeople.png](getpeople.png)
+
+GET people/{id}
+![get-people-id.png](get-people-id.png)
--- handleCreate/index.js
+++ handleCreate/index.js
@@ -1,15 +1,31 @@
+const dynamoose = require('dynamoose');
+const schema = new dynamoose.Schema({
+    "id": String,
+    "name": String
+});
 
+// Define model
+const peopleModel = dynamoose.model('people', schema);
+// Lambda function handler for creating items
+exports.handler = async (event) => {
+    console.log('Event received:', event);
 
-exports.createHandler = async (event) => {
-    const requestBody = JSON.parse(event.body);
+    // Parse the request body
+    const requestBody = event.body;
+
+    console.log('Request body:', requestBody);
 
     try {
-        const newItem = await peopleModel.create(requestBody);
+        // Create a new item in the DynamoDB table
+        const newItem = await peopleModel.create(JSON.parse(requestBody));
+
+        // Return success response with the newly created item
         return {
             statusCode: 201,
             body: JSON.stringify(newItem)
         };
     } catch (error) {
+        // Return error response if creation fails
         return {
             statusCode: 500,
             body: JSON.stringify({ message: error.message })
